fc672106a77a3ef05b79d051e232a488506136ab
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-05-23 23:38:33 +0200

    Add script to fix PROD hyperlinks

---
+++ codes/scripts/fixprods.sh
@@ -0,0 +1,27 @@
+# Replace e.g. \PROD{LM\_RSRF\_RAW} with \PROD{LM_RSRF\_RAW}
+#sed -E 's|(\\PROD\{[a-zA-Z_]+)\\_|\1_|g' -i *.tex
+sed -E 's|(\\[A-Z]+\{[0-9a-zA-Z_]+)\\_|\1_|g' -i *.tex
+# And again to get to \PROD{LM_RSRF_RAW}
+sed -E 's|(\\[A-Z]+\{[0-9a-zA-Z_]+)\\_|\1_|g' -i *.tex
+# And twice more to get them all
+sed -E 's|(\\[A-Z]+\{[0-9a-zA-Z_]+)\\_|\1_|g' -i *.tex
+sed -E 's|(\\[A-Z]+\{[0-9a-zA-Z_]+)\\_|\1_|g' -i *.tex
+sed -E 's|(\\[A-Z]+\{[0-9a-zA-Z_]+)\\_|\1_|g' -i *.tex
+
+
+# Replace \FITS{SOME_DATA_ITEM} with \PROD{SOME_DATA_ITEM}
+# TODO: What if it should be a STATCALIB or so?
+sed -E 's|(dataitem:[0-9a-zA-Z_]*]\{\\)FITS|\1PROD|g' -i *.tex
+
+
+# Ensure all dataitem links are like dataitem:badpix_map_2rg.
+# First copy the name into the hyperref, matches \hyperref[dataitem:n_lss_trace]{\PROD{N_LSS_TRACE}}
+#sed -E 's|dataitem:[0-9a-zA-Z_]*]\{\\PROD\{([0-9a-zA-Z_]+)|dataitem:\1]\{\\PROD\{\1|g' -i *.tex
+sed -E 's|dataitem:[0-9a-zA-Z_]*]\{\\([A-Z]+)\{([0-9a-zA-Z_]+)|dataitem:\2]\{\\\1\{\2|g' -i *.tex
+# Then copy the name into the label
+sed -E 's|([A-Z]+)\{([0-9a-zA-Z_]*)}}}\\label\{dataitem:[0-9a-zA-Z_:]*}|\1\{\2}}}\\label\{dataitem:\2}|g' -i *.tex
+sed -E 's|([A-Z]+)\{([0-9a-zA-Z_]*)}}}\\label\{drsstructure:[0-9a-zA-Z_:]*}|\1\{\2}}}\\label\{drsstructure:\2}|g' -i *.tex
+# Then make all the labels lowercase
+sed -E 's|(dataitem:[0-9a-zA-Z_]*)|\L\1|g' -i *.tex
+
+
