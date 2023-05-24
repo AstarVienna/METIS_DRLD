95fe88723719a11fa0c886dedff0eab18156a58d
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-05-24 07:07:18 +0200

    Set labels for data items

--- codes/scripts/fixprods.sh
+++ codes/scripts/fixprods.sh
@@ -21,7 +21,14 @@ sed -E 's|dataitem:[0-9a-zA-Z_]*]\{\\([A-Z]+)\{([0-9a-zA-Z_]+)|dataitem:\2]\{\\\
 # Then copy the name into the label
 sed -E 's|([A-Z]+)\{([0-9a-zA-Z_]*)}}}\\label\{dataitem:[0-9a-zA-Z_:]*}|\1\{\2}}}\\label\{dataitem:\2}|g' -i *.tex
 sed -E 's|([A-Z]+)\{([0-9a-zA-Z_]*)}}}\\label\{drsstructure:[0-9a-zA-Z_:]*}|\1\{\2}}}\\label\{drsstructure:\2}|g' -i *.tex
+
+# Convert all PRODs that are not yet a link. That is, " \PROD{SOMETHING}"
+# Run manually
+#sed -E 's! \\(PROD|RAW|STATCALIB|EXTCALIB)\{([0-9a-zA-Z_]+)}! \\hyperref\[dataitem:\2]\{\\\1\{\2}}!g' -i *.tex
+
 # Then make all the labels lowercase
 sed -E 's|(dataitem:[0-9a-zA-Z_]*)|\L\1|g' -i *.tex
 
 
+
+# TODO, e.g. "(\PROD{SOMETHING})"
